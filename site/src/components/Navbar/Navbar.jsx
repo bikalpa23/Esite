ea5e1c32e582c26728e750a22085f9d2f87e6bc4
Bikalpa Sangat <bikalpasangat1@gmail.com>
2024-02-04 22:25:46 +0545

    adding Assets folder

--- site/src/components/Navbar/Navbar.jsx
+++ site/src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
         <div className='navbar'>
             <div className="nav-logo">
                 <img src={logo} alt="" />
-                <p>SHOP</p>
+                <p>E-SHOP</p>
             </div>
             <ul className="nav-menu">
                 <li>Shop</li>

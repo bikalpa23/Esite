6da09a25819984dadf8476e78562cb1cf5738880
Bikalpa Sangat <bikalpasangat1@gmail.com>
2024-02-04 22:18:32 +0545

    adding Assets folder

--- site/src/components/Navbar/Navbar.jsx
+++ site/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
-import './Navbar.css'
-
-import logo from '../Assets/logo.png'
-import cart_logo from '../Assets/cart'
+import "./Navbar.css"
+import logo from "../Assets/logo.png"
+import cart_icon from "../Assets/cart_icon.png"
 const Navbar = () => {
-  return (
-    <div className='navbar'>
-        <div className="nav-logo">
-            <img src='{cart_icon}' alt=''/>
-            <p></p>
+    return (
+        <div className='navbar'>
+            <div className="nav-logo">
+                <img src={logo} alt="" />
+                <p>SHOP</p>
+            </div>
+            <ul className="nav-menu">
+                <li>Shop</li>
+                <li>Men</li>
+                <li>Women</li>
+                <li>Kids</li>
+            </ul>
+            <div className="nav-login-cart">
+                <button>Login</button>
+                <img src={cart_icon} alt="" />
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar

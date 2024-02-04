19fc82458a332bd26dfd02f3faa953fd1f91436f
Bikalpa Sangat <bikalpasangat1@gmail.com>
2024-02-04 21:50:50 +0545

    adding Assets folder

--- site/src/components/Navbar/Navbar.jsx
+++ site/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import './Navbar.css'
+
+import logo from '../Assets/logo.png'
+import cart_logo from '../Assets/cart'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+        <div className="nav-logo">
+            <img src='{cart_icon}' alt=''/>
+            <p></p>
+        </div>
+    </div>
   )
 }
 

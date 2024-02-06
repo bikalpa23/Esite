4be154c795188c9a7efc6f871908dc12159c83de
Bikalpa Sangat <bikalpasangat1@gmail.com>
2024-02-06 12:13:48 +0545

    adding pages

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
+import Shop from './Pages/Shop'
 import Navbar from './components/Navbar/Navbar'
+import{BrowserRouter,Routes,Route} from 'react-router-dom'
 
 function App() {
   
   return (
     <>
+    <BrowserRouter>
      <Navbar/>
+    <Routes>
+      <Route path='/' element={<Shop/>}/>
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
